bfccbd4f53c87f0637ea024397ea061e27d167dd
Aryan39232 <20bcs097@iiitdwd.ac.in>
2025-05-28 23:29:31 +0530

    doen

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({ baseURL: 'https://memories-5kr5ijxmz-aryan-patels-projects-a0a4ed79.vercel.app' });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
